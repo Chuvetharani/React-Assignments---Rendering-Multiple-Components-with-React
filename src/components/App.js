19d283a02b86cda002239f209c182bdac87ee14e
Chuvetharani <68391363+Chuvetharani@users.noreply.github.com>
2020-11-07 23:07:51 +0530

    multiple-component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <></>
-  )
-}
-
+    <>
+      <div>
+        <h2 data-ns-test="project-name">Movies</h2>
+        <p data-ns-test="project-description">Comedy, Horror, Romance</p>
+      </div>
+      <div>
+        <h2 data-ns-test="project-name">Birds</h2>
+        <p data-ns-test="project-description">Peacock, Parrot, Dove</p>
+      </div>
+    </>
+  );
+};
 
 export default App;
